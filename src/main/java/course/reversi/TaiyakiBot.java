a5765715854b162f964623c6a034eb4a76672350
Great GIlbert Soco <gilsocojp@gmail.com>
2026-06-24 11:40:51 +0900

    Implemented evauate board

--- src/main/java/course/reversi/TaiyakiBot.java
+++ src/main/java/course/reversi/TaiyakiBot.java
@@ -34,5 +34,40 @@ public class TaiyakiBot extends SimpleBot {
         return convertMove(bestMove);
     }
 
+    private int alphaBeta(char[][] board, int depth, int alpha, int beta, boolean isMaximizing, char botPlayer){
 
+        char opponent = (botPlayer == BLACK) ? WHITE : BLACK;
+        char currentPlayer = isMaximizing ? botPlayer : opponent;
+
+        List<String> validMoves = Reversi.getValidMoves(board, currentPlayer);
+
+        // Base case: leaf node or game over
+        if (depth == 0 || validMoves.isEmpty()){
+            return evaluateBoard(board, botPlayer);
+        }
+    }
+
+    private int evaluateBoard(char[][] board, char botPlayer) {
+        char opponent = (botPlayer == BLACK) ? WHITE : BLACK;
+        int score = 0;
+
+        // Positional Matrix Weighting
+
+        for (int r = 0; r < 8; r++){
+
+            for (int c = 0; c < 8; c++){
+
+                if (board[r][c] == botPlayer) score += BOARD_VALUE_MATRIX[r][c];
+                else if (board[r][c] == opponent) score -= BOARD_VALUE_MATRIX[r][c];
+
+            }
+        }
+
+        // Mobility Advantage for Mid-Game
+        int botMoves = Reversi.getValidMoves(board, botPlayer).size();
+        int oppMoves = Reversi.getValidMoves(board, opponent).size();
+        score += (botMoves - oppMoves) * 15;
+
+        return score;
+    }
 }
